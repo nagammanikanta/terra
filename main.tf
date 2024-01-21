fb79bfc053c29a3560d7dfce4e7497d69e4bf532
nagammanikanta <nagammanikanta@gmail.com>
2024-01-21 11:28:56 +0530

    init

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "google_storage_bucket" "gcs-bucket-cs" {
   name          =var.gcs-bucket-cs
   location      = var.location
@@ -99,4 +99,3 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     ]
   }
 }
-*/
